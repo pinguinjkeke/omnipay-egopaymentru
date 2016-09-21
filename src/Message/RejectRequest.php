1fee5b3c5789c21d7cc4bd3a7a8c8bbd31533b68
pinguinjkeke <yaavakov@gmail.com>
2016-09-21 17:59:36 +0300

    payment_id added to RejectRequest

--- src/Message/RejectRequest.php
+++ src/Message/RejectRequest.php
@@ -8,6 +8,27 @@ use SoapClient;
 class RejectRequest extends SoapAbstractRequest
 {
     /**
+     * Get payment id
+     *
+     * @return string
+     */
+    public function getPaymentId()
+    {
+        return $this->getParameter('payment_id');
+    }
+
+    /**
+     * Set payment id
+     *
+     * @param string $paymentId
+     * @return \Omnipay\Common\Message\AbstractRequest
+     * @throws \Omnipay\Common\Exception\RuntimeException
+     */
+    public function setPaymentId($paymentId)
+    {
+        return $this->setParameter('payment_id', $paymentId);
+    }
+    /**
      * Runs SOAP request
      *
      * @param SoapClient $soapClient
@@ -17,6 +38,23 @@ class RejectRequest extends SoapAbstractRequest
     protected function runTransaction(SoapClient $soapClient, $data)
     {
         return $soapClient->reject($data);
+    }
+
+    /**
+     * Get the raw data array for this message. The format of this varies from gateway to
+     * gateway, but will usually be either an associative array, or a SimpleXMLElement.
+     *
+     * @return mixed
+     * @throws \Omnipay\Common\Exception\InvalidRequestException
+     */
+    public function getData()
+    {
+        $this->validate('shop_id', 'order_id', 'user', 'password', 'payment_id');
+        
+        return array(
+            'order' => array('shop_id' => $this->getShopId(), 'number' => $this->getOrderId()),
+            'payment_id' => $this->getPaymentId()
+        );
     }
 
     /**
