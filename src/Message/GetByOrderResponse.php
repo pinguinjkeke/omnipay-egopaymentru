c8719f50bb43eae2a6a0fae6a2315c6d84232e6c
pinguinjkeke <yaavakov@gmail.com>
2016-09-21 19:24:43 +0300

    isSuccessful method for GetByOrderResponse (status)

--- src/Message/GetByOrderResponse.php
+++ src/Message/GetByOrderResponse.php
@@ -4,5 +4,14 @@ namespace Omnipay\EgopayRu\Message;
 
 class GetByOrderResponse extends SoapResponse
 {
-
+    /**
+     * Is the response successful?
+     * In most cases if response is an array then it's successful
+     *
+     * @return boolean
+     */
+    public function isSuccessful()
+    {
+        return is_array($this->data);
+    }
 }
